0c92de094a75c2dbbc8a0e3e3fdcc4fd4f8ac2ef
boo1ean <boo1ean0807@gmail.com>
2014-02-03 11:37:58 +0200

    Add bunch of helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,35 @@
-var vovels = 'eyuioa';
-var consonants = 'qwrtpasdfghjklmnbvcxz';
 
+var vovels = 'eyuioa';
 module.exports.is_vovel = function(sym) {
 	return vovels.indexOf(sym.toLowerCase()) !== -1;
 };
 
+var consonants = 'qwrtpasdfghjklmnbvcxz';
 module.exports.is_consonant = function(sym) {
 	return consonants.indexOf(sym.toLowerCase()) !== -1;
+};
+
+var brackets = '{}()[]<>';
+module.exports.is_bracket = function(sym) {
+	return brackets.indexOf(sym.toLowerCase()) !== -1;
+};
+
+var open_brackets = '{([<';
+module.exports.is_open_bracket = function(sym) {
+	return open_brackets.indexOf(sym.toLowerCase()) !== -1;
+};
+
+var close_brackets = '})]>';
+module.exports.is_close_bracket = function(sym) {
+	return close_brackets.indexOf(sym.toLowerCase()) !== -1;
+};
+
+var digits = '0123456789';
+module.exports.is_digit = function(sym) {
+	return digits.indexOf(sym.toLowerCase()) !== -1;
+};
+
+var punctuations = ',.!;:-?';
+module.exports.is_punctuation = function(sym) {
+	return punctuations.indexOf(sym.toLowerCase()) !== -1;
 };
--- test/helpers.js
+++ test/helpers.js
@@ -12,4 +12,34 @@ describe('Symbol helpers', function() {
 			symbols.is_consonant(letter).should.be.ok;
 		});
 	});
+
+	it('Should detect brackets', function() {
+		'{}()[]<>'.split('').forEach(function(letter) {
+			symbols.is_bracket(letter).should.be.ok;
+		});
+	});
+
+	it('Should detect open brackets', function() {
+		'{([<'.split('').forEach(function(letter) {
+			symbols.is_open_bracket(letter).should.be.ok;
+		});
+	});
+
+	it('Should detect close brackets', function() {
+		'})]>'.split('').forEach(function(letter) {
+			symbols.is_close_bracket(letter).should.be.ok;
+		});
+	});
+
+	it('Should detect digits', function() {
+		'0123456789'.split('').forEach(function(letter) {
+			symbols.is_digit(letter).should.be.ok;
+		});
+	});
+
+	it('Should detect punctuation', function() {
+		',.!;:-?'.split('').forEach(function(letter) {
+			symbols.is_punctuation(letter).should.be.ok;
+		});
+	});
 });
